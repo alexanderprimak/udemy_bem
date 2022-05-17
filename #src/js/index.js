2e0bda8829dc45527f3ed38a781794651079b22c
Alex <kiborg967@gmail.com>
2022-05-17 13:19:43 +0300

    add fonts and a few remove fonts

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,14 +1,20 @@
+/// work with slider
+
 $(document).ready(function () {
 	$('.carousel__wrapper').slick({
 		prevArrow: `<button type="button" class="carousel__arrow-left"><img src="img/icons/arrow_l.png" alt="arrow"></button>`,
 		nextArrow: `<button type="button" class="carousel__arrow-right"><img src="img/icons/arrow_r.png" alt="arrow"></button>`,
 	})
 })
+
+//// work with catalog
+
 const content = document.querySelectorAll('.catalog__content')
 const contentItem = document.querySelectorAll('.content-item')
 const tabs = document.querySelectorAll('.catalog__tab')
 const wrapperInfo = document.querySelectorAll('.content-item__wrapper-info')
 const wrapper = document.querySelectorAll('.content-item__wrapper')
+
 const changeInnerMoreLink = e => {
 	e.target.innerHTML === 'ПОДРОБНЕЕ'
 		? (e.target.innerHTML = 'НАЗАД')
@@ -33,7 +39,6 @@ const closeTextInfo = () => {
 contentItem.forEach(item => {
 	const [wrapper, wrapperInfo, link] = item.children
 	link.addEventListener('click', e => {
-		e.preventDefault()
 		changeInnerMoreLink(e)
 		wrapper.classList.toggle('content-item__wrapper_disable')
 		wrapperInfo.classList.toggle('content-item__wrapper-info_disable')
@@ -58,16 +63,50 @@ tabs.forEach(tab => {
 
 const buttons = document.querySelectorAll('button')
 const overlays = document.querySelectorAll('.overlay')
+const close = document.querySelectorAll('.modal__close')
 
-const overlayActive = data => {
+const changeDescModal = (overlay, value) => {
+	const [modal] = overlay.children
+	const [close, title, desc] = modal.children
+	desc.innerHTML = value
+}
+
+const overlayShow = (attributeBtn, value) => {
 	overlays.forEach(overlay => {
-		const [dataOver] = overlay.attributes
-		data === dataOver.value ? overlay.classList.add('overlay_active') : null
+		const [attributeModal] = overlay.attributes
+		attributeModal.value === 'thank' ? changeDescModal(overlay, value) : null
+		attributeBtn === attributeModal.value
+			? overlay.classList.add('overlay_active')
+			: null
 	})
 }
+
+const overlayClose = () => {
+	overlays.forEach(overlay => {
+		overlay.classList.remove('overlay_active')
+	})
+}
+
+const getElements = e => {
+	const [content_item] = e.target.parentElement.parentElement.children
+	const [img, sub_title] = content_item.children
+	const [attributeBtn] = e.target.attributes
+	const title = sub_title.innerHTML
+	const value = attributeBtn.value
+
+	return { title, value }
+}
+
 buttons.forEach(btn => {
-	btn.addEventListener('click', e => {
-		const [data] = e.target.attributes
-		overlayActive(data.value)
+	btn.addEventListener('click', event => {
+		const elementObject = getElements(event)
+		const { title, value } = elementObject
+		overlayShow(value, title)
+	})
+})
+
+close.forEach(item => {
+	item.addEventListener('click', () => {
+		overlayClose()
 	})
 })
